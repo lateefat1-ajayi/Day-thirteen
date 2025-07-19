cb0bb50cb246ceaf34d717f433c4c2ddb134bc11
lateefat <ajayilateefat09@gmail.com>
2025-07-19 13:30:52 -0700

    fixing backend deploymen issue

--- Backend/routes/Users.js
+++ Backend/routes/Users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/UserController");
+const { registerUser, loginUser } = require("../controllers/userController");
 const router = express.Router();
 
 
